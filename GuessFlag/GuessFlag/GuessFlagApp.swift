7f4b427f1c2a84c015681d51a556c682e6296a39
kotanamy <donat0009@outlook.com>
2022-07-03 15:30:44 +0300

    Guess the flag init

---
+++ GuessFlag/GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by Ярослав Котов on 03.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
