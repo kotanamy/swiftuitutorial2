caefdf02e514e975d995effb0435005d91db14f2
kotanamy <donat0009@outlook.com>
2022-07-03 15:07:04 +0300

    app

---
+++ TipCalc/TipCalc/TipCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalcApp.swift
+//  TipCalc
+//
+//  Created by Ярослав Котов on 03.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TipCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
