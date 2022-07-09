dc27dbd9586c9445e9429d0adcecc72b9d74403e
kotanamy <donat0009@outlook.com>
2022-07-09 21:09:22 +0300

    ExpensesApp

---
+++ Expenses/Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Ярослав Котов on 09.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
